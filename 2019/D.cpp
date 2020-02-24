261e2361d20bfcabe0c317a1146a8061a94cabc9
Ken Mercado <mightymercado@gmail.com>
2020-02-25 00:19:24 +0800

    2019 Prob D

---
+++ 2019/D.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+
+char rate[10];
+const char* out[] = {
+    "OK",
+    "OK IF ACCOMPANIED",
+    "ACCESS DENIED"
+};
+int main() {
+    int a;
+    scanf("%d %s", &a, rate);
+    if (rate[0] == 'G') {
+        puts(out[0]);
+    } else if (rate[0] == 'P') {
+        if (a < 13) {
+            puts(out[1]);
+        } else {
+            puts(out[1]);
+        }
+    } else if (rate[3] == '3') {
+        if (a < 13) {
+            puts(out[2]);
+        } else {
+            puts(out[0]);
+        }
+    } else if (rate[3] == '6') {
+        if (a < 16) {
+            puts(out[2]);
+        } else {
+            puts(out[0]);
+        }  
+    } else if (rate[3] == '8') {
+        if (a < 18) {
+            puts(out[2]);
+        } else {
+            puts(out[0]);
+        }
+    }
+}
